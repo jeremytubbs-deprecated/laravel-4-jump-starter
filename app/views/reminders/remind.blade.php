f42ae66ecceb7d1552c12ad06d7aa00e2e3920f3
Jeremy Tubbs <jeremytubbs@gmail.com>
2014-07-06 09:29:18 -0400

    Clean up auth views indentation and styles

--- app/views/reminders/remind.blade.php
+++ app/views/reminders/remind.blade.php
@@ -2,24 +2,26 @@
 
 @section('content')
 <div class="container">
-<div class="row">
-<div class="col-lg-4 col-lg-offset-4">
-    <h1>Reset Your Password</h1>
+    <div class="row">
+        <div class="col-lg-4 col-lg-offset-4">
+            <h1>Reset Your Password</h1>
 
-    {{ Form::open(array('action' => 'RemindersController@postRemind', 'method' => 'POST', 'name' => 'form', 'novalidate' => '', 'autocomplete' => 'off')) }}
-        <div class="form-group">
-            <label for="email" class="control-label">Email address</label>
-            {{ Form::email('email', NULL, array(
-                'class' => 'form-control',
-                'placeholder' => 'email@example.com',
-                'ng-model' => 'formData.email',
-                'required' => true
-            ))}}
-        </div>
+            {{ Form::open(array('action' => 'RemindersController@postRemind', 'method' => 'POST', 'name' => 'form', 'novalidate' => '', 'autocomplete' => 'off')) }}
+                <div class="form-group">
+                    <label for="email" class="control-label">Email address</label>
+                    {{ Form::email('email', NULL, array(
+                        'class' => 'form-control',
+                        'placeholder' => 'email@example.com',
+                        'ng-model' => 'formData.email',
+                        'required' => true
+                    ))}}
+                </div>
 
-        <div class="form-group">
-            <button type="submit" class="btn btn-block btn-facebook" ng-disabled="form.$invalid">Send Reset</button>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-block" ng-disabled="form.$invalid">Send Reset</button>
+                </div>
+            {{ Form::close() }}
         </div>
-    {{ Form::close() }}
+    </div>
 </div>
 @stop
--- app/views/sessions/create.blade.php
+++ app/views/sessions/create.blade.php
@@ -2,37 +2,39 @@
 
 @section('content')
 <div class="container">
-<div class="row">
-<div class="col-lg-4 col-lg-offset-4">
-<h1>Welcome.</h1>
+    <div class="row">
+        <div class="col-lg-4 col-lg-offset-4">
+            <h1>Welcome.</h1>
 
-{{ Form::open(array('route' => 'sessions.store', 'id' => 'login-form', 'name' => 'form', 'autocomplete' => 'off', 'novalidate' => '')) }}
-<div class="form-group">
-    <label for="email" class="control-label">Email address</label>
-    {{ Form::email('email', NULL, array(
-		'class' => 'form-control',
-		'placeholder' => 'email@example.com',
-		'ng-model' => 'formData.email',
-        'required' => true
-	))}}
-</div>
+            {{ Form::open(array('route' => 'sessions.store', 'id' => 'login-form', 'name' => 'form', 'autocomplete' => 'off', 'novalidate' => '')) }}
 
-<div class="form-group">
-    <label for="password" class="control-label">Password</label>
-    {{ Form::password('password', array(
-        'class' => 'form-control',
-        'ng-model' => 'formData.password',
-        'required' => true
-    ))}}
-</div>
+            <div class="form-group">
+                <label for="email" class="control-label">Email address</label>
+                {{ Form::email('email', NULL, array(
+            		'class' => 'form-control',
+            		'placeholder' => 'email@example.com',
+            		'ng-model' => 'formData.email',
+                    'required' => true
+            	))}}
+            </div>
 
-<button type="submit" class="btn btn-block" ng-disabled="form.$invalid">Login</button>
-{{ Form::close() }}
-<br>
-<div class="form-group">
-<button type="submit" class="btn btn-block btn-link btn-sm"><a href={{ URL::to('remind') }}>Forgot your password?</a></button>
-</div>
-</div>
-</div>
+            <div class="form-group">
+                <label for="password" class="control-label">Password</label>
+                {{ Form::password('password', array(
+                    'class' => 'form-control',
+                    'ng-model' => 'formData.password',
+                    'required' => true
+                ))}}
+            </div>
+
+            <button type="submit" class="btn btn-block" ng-disabled="form.$invalid">Login</button>
+            {{ Form::close() }}
+
+            <br>
+            <div class="form-group">
+                <button type="submit" class="btn btn-block btn-link btn-sm"><a href={{ URL::to('remind') }}>Forgot your password?</a></button>
+            </div>
+        </div>
+    </div>
 </div>
 @stop
